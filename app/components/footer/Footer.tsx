f92b7863a598e50bd90549bf10326317c78f3c12
ilhamfirdaa <ilhamfirdaa@gmail.com>
2024-05-25 00:00:04 +0700

    feat: add footer component

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      <span>© 2024 by Ilham Firdaus</span>
+    </footer>
+  );
+};
+
+export default Footer;
